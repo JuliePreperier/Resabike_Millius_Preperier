cda300497a25e8b43235ab43dc9e54c5cae95d41
JuliePreperier <julie.preperier@netplus.ch>
2017-10-20 16:22:14 +0200

    import 2.4 --> superadmin /newZone

--- routes/superadmin.js
+++ routes/superadmin.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var zoneModule = require('../modules/zone');
+var loginModule = require('../modules/login');
 
 /* GET superadmin page . */
 router.get('/', function(req, res, next) {
     res.render('superadmin');
 });
+
+/* POST new zone */
+router.post('/newZone', function(req, res, next){
+    zoneModule.insertZone(req.body).then((zone) =>{
+        loginModule.insertLoginFromSuperAdmin(req.body,zone).then(() =>{
+            res.redirect('/superadmin');
+        })
+    })
+});
+
 
 module.exports = router;
