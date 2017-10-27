106d31bc7ccbd2c6daa4f239911a100a3da73641
SandyMillius <sandy.millius@students.hevs.ch>
2017-10-27 11:53:04 +0200

    Visuel amélioré 1

--- public/javascripts/selectquery.js
+++ public/javascripts/selectquery.js
@@ -2,4 +2,8 @@
 $(document).ready(function () {
     console.log("ok");
     $('select').material_select();
+    $(".button-collapse").sideNav();
+    $('.modal').modal({
+        dismissible:false
+    });
 });
